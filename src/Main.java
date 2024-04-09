fcd7c39ca9ecb3e4eb0800f5cac4b20ee3f8c822
Аскат Наринбетов <231308@astanait.edu.kz>
2024-04-09 22:46:05 +0500

    Fifth Task

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void ReverseOrder(int n) {
+    public static void ReverseOrder(Scanner scanner,int n) {
         if (n == 0) {
             return;
         }
-        Scanner scanner = new Scanner(System.in);
-        String enter = scanner.nextLine();
-        ReverseOrder(n - 1);
+        int enter = scanner.nextInt();
+        ReverseOrder(scanner, n - 1);
         System.out.println(enter);
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        ReverseOrder(n);
+        ReverseOrder(scanner,n);
     }
 }
