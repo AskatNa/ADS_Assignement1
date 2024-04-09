853cb96c64037762bf77e6be04c2f88d026c8407
Аскат Наринбетов <231308@astanait.edu.kz>
2024-04-09 22:22:09 +0500

    Third Task

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Main {
-    public static int SumOfSquares(int n) {
-        if(n == 1) {
-            return 1;
-        }else{
-            return n * n + SumOfSquares(n - 1);
+    public static int SumOfPositive(int n){
+        if(n < 0){
+            return -1;
+        }
+        else{
+            return n + SumOfPositive(n - 1);
         }
     }
     public static void main(String[] args) {
-        int  result = SumOfSquares(4);
-        System.out.println("Result: " + result);
-
+        int result = SumOfPositive(4);
+        System.out.println(result);
     }
 }
