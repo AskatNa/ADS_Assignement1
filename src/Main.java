03e5fb72e39e67504e1f0535f84901b9d366e9c9
Аскат Наринбетов <231308@astanait.edu.kz>
2024-04-10 09:08:28 +0500

    Fifth Task

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void isPowerOfTwo(int n) {
-        if (n <= 0) {
-            System.out.println("Can`t be negative");
-        } else if (n == 1) {
-            System.out.println("Power of two");
-        } else if (n % 2 == 0) {
-            System.out.println("Power of two");
-        } else {
-            System.out.println("Not the power of two");
-        }
-    }
-
+    public static void ReverseOrder(int n) {
+        if (n == 0) {
+            return;
+        }else{
+        Scanner scanner = new Scanner(System.in);
+        String enter = scanner.nextLine();
+        ReverseOrder(n - 1);
+        System.out.println(enter);
+    }}
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        isPowerOfTwo(number);
-
+        int n = scanner.nextInt();
+        ReverseOrder(n);
     }
 }
