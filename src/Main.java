79217ad2494fd589ca66eb6c55c6485baf3e352c
Аскат Наринбетов <231308@astanait.edu.kz>
2024-05-01 13:03:21 +0500

    Ninth Task

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,28 @@
-import java.util.Queue;
-import java.util.Scanner;
-
 public class Main {
-    public static boolean isPowerOfTwo(int n) {
-        if (n < 0) {
-            return false;
-        } else if (n == 1) {
-            return true;
-        } else if (n % 2 == 0) {
-            return isPowerOfTwo(n / 2);
-        } else {
-            return false;
+    public static void main(String[] args) {
+        String input1 = "AB";;
+        printPermutations(input1.toCharArray(), 0);
+
+        String input2 = "IOX";
+        printPermutations(input2.toCharArray(), 0);
+    }
+
+    public static void printPermutations(char[] chars, int currentIndex) {
+        if (currentIndex == chars.length - 1) {
+            System.out.println(String.valueOf(chars));
+            return;
+        }
+
+        for (int i = currentIndex; i < chars.length; i++) {
+            swap(chars, currentIndex, i);
+            printPermutations(chars, currentIndex + 1);
+            swap(chars, currentIndex, i);
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        isPowerOfTwo(number);
-        
+    public static void swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
     }
 }
